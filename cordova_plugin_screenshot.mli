49281980735d1f7286ad0cfbc459c24fb074b3bb
Ke <Ke.Douglas.Wang@hotmail.com>
2017-08-31 17:59:14 +0200

    change the interface of the callback function

--- cordova_plugin_screenshot.mli
+++ cordova_plugin_screenshot.mli
@@ -10,24 +10,24 @@ module Response_file : sig
 end
 
 val save :
-  ?callback: (string option -> Response_file.t ->  unit) ->
-  ?format: string                                        ->
-  ?quality: int                                          ->
-  filename: string                                       ->
+  ?callback: (string option -> Response_file.t -> string Lwt.t) ->
+  ?format: string                                               ->
+  ?quality: int                                                 ->
+  filename: string                                              ->
   unit
 [@@js.global "navigator.screenshot.save"]
 
 val uri :
-  ?callback: (string option -> Response_uri.t ->  unit) ->
-  ?quality: int                                         ->
-  unit                                                  ->
+  ?callback: (string option -> Response_uri.t -> string Lwt.t) ->
+  ?quality: int                                                ->
+  unit                                                         ->
   unit
 [@@js.global "navigator.screenshot.URI"]
 
 val uri_sync : 
-  ?callback: (string option -> Response_uri.t -> unit) ->
-  ?quality: int                                        ->
-  unit                                                 ->
+  ?callback: (string option -> Response_uri.t -> string Lwt.t) ->
+  ?quality: int                                                ->
+  unit                                                         ->
   unit
 [@@js.global "navigator.screenshot.URISync"]
 (* -------------------------------------------------------------------------- *)
